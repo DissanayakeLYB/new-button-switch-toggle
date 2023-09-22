c8b79095ab209a02d00682719b99c8c180bc59f0
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2023-09-22 22:09:44 +0530

    2

---
+++ index.js
@@ -0,0 +1,12 @@
+function greet(firstName, lastName) {
+    console.log('Hello ' + firstName + ' ' + lastName);
+}
+
+
+greet('Lasith', 'Dissanyake');
+
+function square(num){
+    return num*num;
+}
+
+console.log(square(2));
